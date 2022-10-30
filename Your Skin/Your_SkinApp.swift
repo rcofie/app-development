30d4f07cf2b73fe1723ac8c187051779094f786d
Rebecca Cofie <rebeccacofie@hotmail.co.uk>
2022-10-30 13:58:02 +0000

    Initial Commit

---
+++ Your Skin/Your_SkinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Your_SkinApp.swift
+//  Your Skin
+//
+//  Created by Rebecca Cofie on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Your_SkinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
